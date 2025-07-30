c3455fcd0c5891efcec00cfed59890ac043d8146
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-07-30 16:10:31 +0100

    add mood model

---
+++ lib/core/models/mood_model.dart
@@ -0,0 +1,11 @@
+class MoodModel {
+  final String moodName;
+  final String animationPath;
+  final String category;
+
+  MoodModel({
+    required this.moodName,
+    required this.animationPath,
+    required this.category,
+  });
+}
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: LiquidSwipe.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Container();
+        },
+      ),
+    );
   }
 }
