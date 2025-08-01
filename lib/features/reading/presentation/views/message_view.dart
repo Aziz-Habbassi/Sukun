2ab37fad0b411c20a90833aac4612cb496b15b16
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-08-01 15:16:44 +0100

    message view done

--- lib/features/reading/presentation/views/message_view.dart
+++ lib/features/reading/presentation/views/message_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ReadingView extends StatelessWidget {
   const ReadingView({super.key});
@@ -19,7 +20,71 @@ class ReadingView extends StatelessWidget {
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("I hope you are fine ")],
+          children: [
+            Text(
+              "تذكّر، في كل شعور رسالة من الله",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "Lateef",
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+              textDirection: TextDirection.rtl,
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * .1,
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    textDirection: TextDirection.rtl,
+                    "احْرِصْ على ما يَنفَعُكَ، واسْتَعِنْ بالله ولا تَعْجِزْ",
+                    style: TextStyle(fontFamily: "Arabic", fontSize: 32),
+                  ),
+                  SizedBox(height: 20),
+                  Divider(endIndent: 30, indent: 30),
+                  SizedBox(height: 20),
+
+                  Text(
+                    textAlign: TextAlign.center,
+                    textDirection: TextDirection.rtl,
+                    style: TextStyle(fontFamily: "Arabic", fontSize: 24),
+
+                    "قالها النبي لرجل متردد لا يعرف كيف يختار طريقه. علمه أن يسعى لما فيه فائدة ويطلب العون من الله، ولا يستسلم للتيه",
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            GestureDetector(
+              onTap: () => context.go("/"),
+              child: Container(
+                alignment: Alignment.center,
+                width: 200,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: const Color.fromARGB(255, 0, 135, 245),
+                ),
+                child: Text(
+                  "رجوع",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
